94e99f5387f2f6bb2110dcd04c6a20dd570c31d8
SsL <jakeevancohen@gmail.com>
2026-08-08 23:51:25 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -67,7 +67,7 @@ import logging
 from collections import Counter
 from typing import Optional, TYPE_CHECKING
 
-import _bizhawk as bizhawk
+from . import _bizhawk as bizhawk
 from worlds._bizhawk.client import BizHawkClient
 from NetUtils import ClientStatus
 
